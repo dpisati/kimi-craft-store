0bc33000c899389be71f6c3de0c0fbbb2cc840fb
dpisati <43629737+dpisati@users.noreply.github.com>
2021-01-16 07:51:45 +1300

    join pages Login, Register and Forgot password in one and change between them using  state

--- pages/auth/login.js
+++ pages/auth/login.js
@@ -4,26 +4,61 @@ import LoginHeader from '../../src/components/LoginHeader'
 import Button from '../../src/components/Button'
 import styles from '../../styles/Login.module.css'
 import TextField from '@material-ui/core/TextField';
+import { useState } from 'react'
 
 export default function Login() {
+  const [ loginOrSignUp, setLoginOrSignUp ] = useState("login");
   return (
     <div>
       <Head>
         <title>Kimi Craft</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <LoginHeader active="login" />
+      <LoginHeader active={loginOrSignUp} setLoginOrSignUp={setLoginOrSignUp} />
+
+      {loginOrSignUp === "login" && (
+        <>
+          <div className={styles.loginInputContainer}>
+          <TextField id="email" label="Email Address" fullWidth />
+          <div className={styles.betweenInputs}></div>
+          <TextField id="password" type="password" label="Password" fullWidth />
+          
+            <h3 onClick={() => setLoginOrSignUp("forgot")} className={styles.forgotPassword}>Forgot password?</h3>
+          
+          </div>
+          <div className={styles.loginBtn}>        
+              <Button href="/dashboard" title="Login" />        
+          </div>        
+        </>
+      )}
+      
+      {loginOrSignUp === "register" && (
+        <>
       <div className={styles.loginInputContainer}>
+        <TextField id="name" label="Name" fullWidth />
+        <div className={styles.betweenInputs}></div>
         <TextField id="email" label="Email Address" fullWidth />
         <div className={styles.betweenInputs}></div>
         <TextField id="password" type="password" label="Password" fullWidth />
-        <Link href="/auth/forgot">
-          <h3 className={styles.forgotPassword}>Forgot password?</h3>
-        </Link>
+        <div className={styles.betweenInputs}></div>
+        <TextField id="confirmPassword" type="password" label="Confirm Password" fullWidth />
       </div>
       <div className={styles.loginBtn}>        
-          <Button href="/dashboard" title="Login" />        
-      </div>
+          <Button href="/dashboard" title="Register" />        
+      </div>     
+        </>
+      )}
+
+      {loginOrSignUp === "forgot" && (
+        <>
+          <div className={styles.loginInputContainer}>
+            <TextField id="email" label="Email Address" fullWidth />
+          </div>
+          <div className={styles.loginBtn}>        
+              <Button href="/auth/login" title="Reset Password" />        
+          </div>
+        </>
+      )}
     </div>
   )
 }
--- src/components/LoginHeader/index.js
+++ src/components/LoginHeader/index.js
@@ -1,21 +1,16 @@
 import Image from 'next/image';
-import Link from 'next/link';
 import styles from './LoginHeader.module.css';
 
-function LoginHeader({ active }) {
+function LoginHeader({ active, setLoginOrSignUp }) {
     return (
       <div className={styles.loginContainer}>
         <div className={styles.logo}>
           <Image src="/kimilogo.png" alt="Logo Image" width="171" height="171"/>
           <Image src="/kimicraft.png" className={styles.kimicraft} alt="Kimi  Craft" width="138" height="111"/>
         </div>
-        <div className={styles.loginOptions}>
-          <Link href="/auth/login">
-            <h3 className={ active === "login" ? styles.active : ""}>Login</h3>
-          </Link>
-          <Link href="/auth/register">
-            <h3 className={ active === "register" ? styles.active : ""}>Register</h3>
-          </Link>
+        <div className={styles.loginOptions}>          
+            <h3 onClick={() => setLoginOrSignUp("login")} className={ active === "login" ? styles.active : ""}>Login</h3>     
+            <h3 onClick={() => setLoginOrSignUp("register")} className={ active === "register" ? styles.active : ""}>Register</h3>
         </div>
       </div>
     );
